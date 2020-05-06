8fbe02be7fb2b696a5bc0a869a91be5e6b12f9a5
Francis Nguyen <hoschn@gmail.com>
2020-05-06 08:34:41 -0400

    Finished UI fix

--- app/src/main/java/com/example/tareservefinal/TaInfo.kt
+++ app/src/main/java/com/example/tareservefinal/TaInfo.kt
@@ -165,17 +165,6 @@ class TaInfo : Fragment() {
                 }
             })
 
-        database.child("Users").child(param1!!).child("TAData").child("Schedule")
-            .addListenerForSingleValueEvent(object: ValueEventListener{
-                override fun onCancelled(databaseError: DatabaseError) {
-                }
-
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    val officeString = dataSnapshot.value.toString()
-                    taTime.text = "Office Hours: $officeString"
-                }
-            })
-
         loadProfilePic(view)
     }
 
